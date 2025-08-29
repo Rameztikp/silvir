496ba4f4ad6c6827245bdb73956c6d35471bbe1e
Ramez Talal <rameeztalal.a.h@email.com>
2025-08-30 01:03:29 +0300

    fix(deploy): Bind to port 10000 on Render

--- server/index.ts
+++ server/index.ts
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
+  const port = parseInt(process.env.NODE_ENV === 'production' ? '10000' : (process.env.PORT || '5000'), 10);
   server.listen({
     port,
     // For Render deployment, we need to bind to 0.0.0.0
